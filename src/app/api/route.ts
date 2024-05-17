6e13187ca930776061dfe3c03fa196d546935b05
Lawrence Hunt <huntlawrence@gmail.com>
2024-05-17 10:46:39 +0700

    add force-dynamic to route

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,3 +1,5 @@
-export async function GET() {
-  // your data-fetching code here!
-}
+// this is a special export that tells the router this is a dynamic route
+export const dynamic = "force-dynamic";
+
+// this is the GET method for this route
+export async function GET() {}
